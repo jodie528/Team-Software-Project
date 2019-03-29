00f045be8e3b627f0c28143f3ac7780933cdcd4a
jodie528 <43610109+jodie528@users.noreply.github.com>
2019-03-29 11:14:06 +0000

    modify view

--- my_web/views.py
+++ my_web/views.py
@@ -5,14 +5,14 @@ import json
 
 def index(request):
     data = {}
-    data["modules"] = list(models.module_info.objects.values())
+    #data["modules"] = list(models.module_info.objects.values())
     # data["modules"] = list(models.module_info.objects.all())
     # data["modules"] = list(models.module_info.objects.values('title', 'code', 'smester_taught'))
-    # modules = models.module_info.objects.all()
-    # data = {"modules": []}
-    # l = len(modules)
-    # for i in range(l):
-    #     data["modules"].append({"name": modules[i].title, "code": modules[i].code, "session": modules[i].smester_taught})
+    modules = models.module_info.objects.all()
+    data = {"modules": []}
+    l = len(modules)
+    for i in range(l):
+        data["modules"].append({"name": modules[i].title, "code": modules[i].code, "session": modules[i].smester_taught})
     # # js_data = json.dumps(data)
     return render(request, 'index.html', {'data': data})
 
